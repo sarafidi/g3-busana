ca06071167bd23b4f793b35c6ea9795310d61544
sarafina-rafidi <sarafina_rafidi@outlook.com>
2026-05-18 18:53:38 +0800

    Initial commit: BUSANA.my Spring Boot boilerplate

---
+++ src/main/java/com/busana/BusanaApplication.java
@@ -0,0 +1,13 @@
+package com.busana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/busana/BusanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
