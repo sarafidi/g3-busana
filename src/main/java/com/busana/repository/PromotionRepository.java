366e2d494c01648aa653b02236979afb9100ecde
zeotical <aceyems10@gmail.com>
2026-06-14 12:16:17 +0800

    Repository (Promotion)

---
+++ src/main/java/com/busana/repository/PromotionRepository.java
@@ -0,0 +1,10 @@
+package com.busana.repository;
+
+
+import com.busana.model.Promotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PromotionRepository extends JpaRepository<Promotion, String> {
+}
