993d85c44f68fe035fb448ae9f412e9733e7f88a
sarafidi <sarafina.rafidi@student.mmu.edu.my>
2026-06-14 20:45:08 +0800

    [modify] ShoppingCartRepository.java

--- src/main/java/com/busana/repository/ShoppingCartRepository.java
+++ src/main/java/com/busana/repository/ShoppingCartRepository.java
@@ -4,9 +4,9 @@ import com.busana.model.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, String> {
-    List<ShoppingCart> findByCustomer_CustomerID(String customerID);
+    Optional<ShoppingCart> findByCustomer_CustomerID(String customerID);
 }
