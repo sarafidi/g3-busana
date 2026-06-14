ead967f96cbef7dc3ba095577dffc74469babc98
sarafidi <sarafina.rafidi@student.mmu.edu.my>
2026-06-14 22:15:26 +0800

    [modify] Add wishlistID to WishlistItem

--- src/main/java/com/busana/model/WishlistItem.java
+++ src/main/java/com/busana/model/WishlistItem.java
@@ -10,19 +10,28 @@ public class WishlistItem {
     private String wishlistItemID;
 
     @ManyToOne
+    @JoinColumn (name = "wishlistID", nullable = false)
+    private Wishlist wishlist;
+
+    @ManyToOne
     @JoinColumn (name = "variantID", nullable = false)
     private ProductVariant variant;
 
     public WishlistItem() { }
 
-    public WishlistItem(String wishlistItemID, ProductVariant variant) {
+    public WishlistItem(String wishlistItemID, Wishlist wishlist, ProductVariant variant) {
         this.wishlistItemID = wishlistItemID;
+        this.wishlist = wishlist;
         this.variant = variant;
     }
 
     public String getWishlistItemID() { return wishlistItemID; }
     public void setWishlistItemID(String wishlistItemID) { this.wishlistItemID = wishlistItemID; }
 
+    public Wishlist getWishlist() { return wishlist; }
+    public void setWishlist(Wishlist wishlist) { this.wishlist = wishlist; }
+
     public ProductVariant getVariant() { return variant; }
     public void setVariant(ProductVariant variant) { this.variant = variant; }
+
 }
