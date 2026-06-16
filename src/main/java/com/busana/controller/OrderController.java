7f9cec131d6e836317d54124e2e2eb1892387265
zeotical <aceyems10@gmail.com>
2026-06-16 13:00:55 +0800

    Order Controller

---
+++ src/main/java/com/busana/controller/OrderController.java
@@ -0,0 +1,88 @@
+package com.busana.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+
+import com.busana.model.Order;
+import com.busana.service.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping
+public class OrderController {
+            private final OrderService orderService;
+
+            public OrderController(OrderService orderService) {
+                this.orderService = orderService;
+            }
+    // Admin View All Orders
+    @GetMapping("/admin/orders")
+    public String viewAllOrders(Model model) {
+        model.addAttribute("pageTitle", "Orders");
+        model.addAttribute("orders", orderService.getAllOrders());
+        return "admin/order-list";
+    }
+
+    // Admin Views Order Details
+    @GetMapping("/admin/orders/{orderId}")
+    public String viewOrderDetails(
+        @PathVariable String orderId,
+        Model model,
+        RedirectAttributes redirectAttributes
+    ) {
+        try {
+            Order order = orderService.getOrderByOrderID(orderId);
+            model.addAttribute("pageTitle", order.getOrderID());
+            model.addAttribute("order", order);
+            return "admin/order-detail";
+        } catch (IllegalArgumentException ex) {
+            redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
+            return "redirect:/admin/orders";
+        }
+    }
+
+    //Customer Views Order Confirmation Page
+    @GetMapping("/customer/order-confirmation/{orderId}")
+    public String viewOrderConfirmationDetails(
+        @PathVariable String orderId,
+        Model model,
+        RedirectAttributes redirectAttributes
+    ) {
+        try {
+            Order order = orderService.getOrderByOrderID(orderId);
+            model.addAttribute("pageTitle", "Order Confirmation for ORder: " + order.getOrderID());
+            model.addAttribute("order", order);
+            return "customer/order-confirmation";
+        } catch (IllegalArgumentException ex) {
+            redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
+            return "redirect:/customer/checkout";
+        }
+    }
+
+    // Admin Updates Order Status
+    @PostMapping("/admin/orders/{orderId}")
+    public String updateOrderStatus(
+        @PathVariable String orderId,
+        // @PathVariable String newStatus,
+        @RequestParam String newStatus,
+        RedirectAttributes redirectAttributes
+    ) {
+        try {
+            orderService.updateOrderStatus(orderId, newStatus);
+            redirectAttributes.addFlashAttribute("successMessage", "Order status updated to: " + newStatus);
+            return "redirect:/admin/orders/" + orderId;
+        } catch (IllegalArgumentException ex) {
+            redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
+            return "redirect:/admin/orders/" + orderId;
+        }
+    }
+
+
+}
+
