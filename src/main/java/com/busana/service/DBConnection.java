999c96964c236bcabc34adf7ac8e7c792c07bdc8
Chuen Hou <kokchuenhou@gmail.com>
2026-06-17 19:30:41 +0800

    UPDATE LOGIN / AUTH MODULE Updated DBConnection to connect to database AuthService.java now use DBConnection to execute sql query AuthService no longer use Spring to execute sql query AuthService no longer use AdminRepository and CustomerRepository

--- src/main/java/com/busana/service/DBConnection.java
+++ src/main/java/com/busana/service/DBConnection.java
@@ -1,13 +1,30 @@
+//Singleton pattern implementation for DBConnection class
+
 package com.busana.service;
 
-import org.springframework.stereotype.Component;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-@Component
+
+
 public class DBConnection {
     private static DBConnection instance;
+    private Connection connection;
+
+    private static final String url = "jdbc:mysql://localhost:3306/busana";
+    private static final String username = "root";
+    private static final String password = "1234";
 
     private DBConnection() {
-       System.out.println("DBConnection instance created.");
+        // System.out.println("==============DBConnection instance created.=================");
+        // connect();
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("==============Connected to the database.=================");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("==============Failed to connect to the database.=================");
+        }
     }
 
     public static DBConnection getInstance() {
@@ -15,5 +32,9 @@ public class DBConnection {
             instance = new DBConnection();
         }
         return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
     }
 }
